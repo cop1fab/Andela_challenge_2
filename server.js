04e0bdecdb4be4acdcc0a6a490e211ade0731dec
Cop1fab <bienaimecopain@gmail.com>
2018-11-29 00:57:18 +0200

    [chore 162292447] set the server

---
+++ server.js
@@ -0,0 +1,29 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import parcelRouter from './src/routes/parcelRoutes';
+import authRouter from './src/routes/authRoutes';
+import userRouter from './src/routes/userRoutes';
+
+dotenv.config();
+const app = express();
+const port = process.env.PORT || 8485;
+
+app.use(express.json());
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+
+  next();
+});
+app.get('/', (req, res) => res.status(200).send({ message: 'Welcome to home SendIT API', status: 200 }));
+
+
+app.use('/api/v1/parcels', parcelRouter);
+app.use('/api/v1/auth', authRouter);
+app.use('/api/v1/users', userRouter);
+
+
+// eslint-disable-next-line no-console
+app.listen(port, () => console.log(`SendIT app listening on port ${port}!`));
+
+export default app;
